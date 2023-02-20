cfbf140cbee54de03d6659109ba419ea2f8b88ec
Walter Salas <waldev.salas@gmail.com>
2023-02-20 16:20:32 -0300

    Add changes on index.tsx and app.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,36 @@
+import { GetServerSidePropsContext } from "next";
 import Head from 'next/head'
+import Image from "next/image"
 
-export default function Home() {
+import { Header } from "@/components/Header";
+import { TopBar } from "@/components/TopBar";
+
+import { HomeHeroCategories } from "@/components/HomeHeroCategories";
+import { Categories } from "@/models/Categories";
+
+import { Box, Container, SimpleGrid } from "@chakra-ui/react";
+import { AdvantageSection } from "@/components/AdvantageSection";
+import { ProductCard } from "@/components/ProductCard";
+
+export type Product = {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+  image: string;
+  rating: {
+    count: number;
+    rate: number;
+  };
+}
+
+type Props = {
+  products: Product[],
+  categories: Categories[]
+}
+
+export default function Home( { products, categories }: Props) {
   return (
     <>
       <Head>
@@ -9,9 +39,43 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      <TopBar />
+      <Box marginBottom="2rem">
+        <Header />
+      </Box>
+
       <main>
-        <h1>Hello World</h1>
+        <Container size={{
+          lg: 'lg'
+        }}>
+          <HomeHeroCategories categories={categories}></HomeHeroCategories>
+          <AdvantageSection />
+
+          {<SimpleGrid minChildWidth='255px' spacing={"1.85rem"}>
+            {products.map(product => {
+              return <ProductCard {...product} key={product.id} />
+            })}
+          </SimpleGrid>}
+        </Container>
+
+        
       </main>
     </>
   )
+}
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const products = await fetch("https://fakestoreapi.com/products")
+    .then(res => res.json())
+  const categories = await fetch("https://fakestoreapi.com/products/categories")
+    .then(res => res.json())
+
+
+  return {
+    props: {
+      products,
+      categories
+    }
+  }
 }
